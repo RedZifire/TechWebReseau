a8db408036445f0e17b2efb9f50c059cb4219f80
elena <elena.martinoni@unine.ch>
2026-05-11 19:32:30 +0200

    commentaires et points d'interrogation

--- projet/backend/app.py
+++ projet/backend/app.py
@@ -15,12 +15,21 @@ class User(db.Model): #cette classe represente un tableau dans le database et ç
     id = db.Column(db.Integer, primary_key=True) #identificateur unique du User: c'est un int (integer), le nombre associé a chaque username 
     username = db.Column(db.String(80), unique=True) #le username doit être unique (max 80 caractères)
     password = db.Column(db.String(80))
+    posts = db.relationship("Post", backref="author") #pour la relation user-post ??????????????????????????????????????????????????????
+    comments = db.relationship("Comment", backref="author") #??????????????????????????????????????????????????????
 
 class Post(db.Model): #autre tableau, crée pour les post
     id = db.Column(db.Integer, primary_key=True)
     image = db.Column(db.String(200)) #sauvgarde le URL de l'image
     caption = db.Column(db.String(200)) #description de l'image
-    user_id = db.Column(db.Integer) #sauvgarde l'id du User qui a créé le post 
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id")) #sauvgarde l'id du User qui a créé le post  ??????????????????????????????????????????????????????
+    comments = db.relationship("Comment", backref="post") #comments sous le post
+
+class Comment(db.Model): #pour créer des commentaires
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(300)) #quoi on a écrit
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id")) #qui a écrit ??????????????????????????????????????????????????????
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id")) #dans quel post ??????????????????????????????????????????????????????
 
 # ---------------- ROUTES ----------------
 #les routes sont les URL du site
@@ -34,10 +43,14 @@ def login():
     if request.method == "POST": #request est un objet qui représente la requeste HTTP arrivé du broswer
         username = request.form.get("username") #on remplace username avec le name choisi par l'utilisateur contenu dans form (request.form contient les données envoyées par le form HTML)
 
-        user = User.query.filter_by(username=username).first() #on cherche cet utent dans le db
+        user = User.query.filter_by(
+            username=username, 
+            password=request.form.get("password")
+        ).first() #on cherche cet utent dans le db et on vérifie que la password soit sorrecte
 
         if user: #si on le trouve
             session["user"] = user.username #on sauvgarde le login dans la session
+            session["user_id"] = user.id  #on sauvgarde le id
             return redirect("/profile") #et on l'evoye à la page profile
 
         return "User not found", 400 
@@ -86,10 +99,29 @@ def add_post():
     post = Post(
         image=request.form.get("image"),
         caption=request.form.get("caption")
+        user_id=session["user_id"] #pour remarquer à qui appartient le post ??????????????????????????????????????????????????????
     ) #création du post (avec les données du form)
 
     db.session.add(post)
     db.session.commit() #ajouter le post au db
+
+    return redirect("/home")
+
+# ADD COMMENTAIRE à UN POST
+@app.route("/add_comment/<int:post_id>", methods=["POST"])
+def add_comment(post_id):
+
+    if "user_id" not in session: #si on a pas accédé on peut pas commenter
+        return redirect("/login")
+
+    comment = Comment(
+        text=request.form.get("text"),
+        user_id=session["user_id"],
+        post_id=post_id
+    ) #creation du commentaire 
+
+    db.session.add(comment)
+    db.session.commit() #ajouter le commentaire au db
 
     return redirect("/home")
 
